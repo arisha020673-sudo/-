d9e69589e8e25ed38df0f120a46f3717772f22d9
arisha020673-sudo <arisha020673@gmail.com>
2025-08-25 16:07:40 +0300

    Update Задание7.py

--- Дз2/Задание7.py
+++ Дз2/Задание7.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv('shop.csv', sep=',', encoding='utf-8')
+orders = pd.read_csv('orders.csv', sep=',', encoding='utf-8')
+customers = pd.read_csv('customers.csv', sep=',', encoding='utf-8')
 
-filter_df = df[(df['birth_date'] >= '1990-01-01') & (df['birth_date'] <= '1990-12-31')]
-print(filter_df[['order_id', 'customer_id']])
+customers['birth_date'] = pd.to_datetime(customers['birth_date'], errors='coerce')
+
+date = customers[customers['birth_date'].dt.year == 1990]
+merged = orders.merge(date[['customer_id']], on='customer_id', how='inner')
+
+result = merged[['order_id', 'customer_id']]
+
+print(result)
